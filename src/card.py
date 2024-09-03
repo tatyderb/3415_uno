a92e39347a4b7e3f3e5494966282facd8275e2e0
tatyderb <tatyderb@gmail.com>
2024-09-03 12:34:59 +0300

    Card: ==, load, можно ли играть одну карту на другую. + tests.

--- src/card.py
+++ src/card.py
@@ -1,8 +1,16 @@
 """Карты UNO."""
+from typing import Self
 
 
 class Card:
+    COLORS = ['r', 'g', 'b', 'y']
+    NUMBERS = list(range(10))
+
     def __init__(self, color: str, number: int):
+        if color not in Card.COLORS:
+            raise ValueError
+        if number not in Card.NUMBERS:
+            raise ValueError
         self.color = color
         self.number = number
 
@@ -10,6 +18,18 @@ class Card:
         # 'r3'
         return f'{self.color}{self.number}'
 
+    def __eq__(self, other):
+        return self.color == other.color and self.number == other.number
+
     def save(self):
         return repr(self)
+
+    @staticmethod
+    def load(text: str):
+        """From 'y3' to Card('y', 3)."""
+        return Card(color=text[0], number=int(text[1]))
+
+    def can_play_on(self, other: Self) -> bool:
+        """Можно ли играть карту self на карту other."""
+        return self.color == other.color or self.number == other.number
 
--- test/test_card.py
+++ test/test_card.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.card import Card
 
 
@@ -15,5 +17,52 @@ def test_save():
     assert repr(c) == 'g7'
     assert c.save() == 'g7'
 
+def test_eq():
+    c1 = Card('y', 3)
+    c2 = Card('y', 3)
+    c3 = Card('y', 1)
+    c4 = Card('g', 3)
+    c5 = Card('b', 7)
 
+    assert c1 == c2
+    assert c1 != c3
+    assert c1 != c4
+    assert c1 != c5
+
+def test_load():
+    s = 'y3'
+    c = Card.load(s)
+    assert c == Card('y', 3)
+
+    s = 'b6'
+    c = Card.load(s)
+    assert c == Card('b', 6)
+
+def test_divzero():
+    # пример теста с ловлей исключения
+    with pytest.raises(ZeroDivisionError):
+        x = 2 / 0
+        # y = 3 / 15
+
+def test_validation():
+    with pytest.raises(ValueError):
+        Card('yellow', 1)
+    with pytest.raises(ValueError):
+        Card('ф', 1)
+    with pytest.raises(ValueError):
+        Card('b', 10)
+    with pytest.raises(ValueError):
+        Card('b', '3')
+
+def test_play_on():
+    c1 = Card.load('y1')
+    c2 = Card.load('y5')
+    c3 = Card.load('g1')
+    c4 = Card.load('g6')
+
+    assert c1.can_play_on(c1)
+    assert c2.can_play_on(c1)
+    assert c1.can_play_on(c2)
+    assert c3.can_play_on(c1)
+    assert not c4.can_play_on(c1)
 
