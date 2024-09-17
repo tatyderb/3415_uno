dfced95d968982f736e1ed02c9eeea0f3aeb4d38
tatyderb <tatyderb@gmail.com>
2024-09-17 14:22:57 +0300

    eq с разными типами

--- src/player.py
+++ src/player.py
@@ -1,3 +1,4 @@
+import json
 import typing
 
 from src.hand import Hand
@@ -12,7 +13,11 @@ class Player:
     def __str__(self):
         return f'{self.name}({self.score}): {self.hand}'
 
-    def __eq__(self, other: typing.Self):
+    def __eq__(self, other: typing.Self | str | dict):
+        if isinstance(other, str):
+            other = self.load(json.loads(other))
+        if isinstance(other, dict):
+            other = self.load(other)
         return self.name == other.name \
                and self.score == other.score \
                and self.hand == other.hand
