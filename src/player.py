444edd93f413dbadd2473d113ed47295c5326f53
Kirikus <kirikusan@gmail.com>
2024-10-02 04:09:11 +0300

    Player - enable __hash__
    For creating {Player: PlayerInteraction} dictionary.

--- src/player.py
+++ src/player.py
@@ -1,3 +1,4 @@
+import hashlib
 import json
 import typing
 
@@ -23,6 +24,10 @@ class Player:
             and self.score == other.score
             and self.hand == other.hand
         )
+
+    def __hash__(self) -> int:
+        # Can't "return self.name", because hash must return an integer
+        return int(hashlib.sha1(self.name.encode("utf-8")).hexdigest(), 16) % (10**8)
 
     def save(self) -> dict:
         return {"name": self.name, "hand": str(self.hand), "score": self.score}
