ebfae225f7c85d751c5196ade68874ef0eb94249
tatyderb <tatyderb@gmail.com>
2024-10-14 22:51:56 +0300

    Green window background

--- src/application.py
+++ src/application.py
@@ -6,17 +6,22 @@ from ui.resource import RESOURCE as RSC
 class Application:
     def __init__(self):
         pygame.init()
+
         self.size = (self.width, self.height) = (RSC['width'], RSC['height'])
+        self.window_rect = pygame.Rect(0, 0, self.width, self.height)
         self.display = pygame.display.set_mode(self.size)
+
         pygame.display.set_caption(RSC['title'])
         icon_img = pygame.image.load(RSC['image']['icon'])
         pygame.display.set_icon(icon_img)
 
     def run(self):
         running = True
+        self.display.fill('darkgreen', (0, 0, self.width, self.height))
+        pygame.display.update(self.window_rect)
         while running:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_qP:
+                if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_q:
                     running = False
 
 
