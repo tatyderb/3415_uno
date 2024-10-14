e17593af7a930d3d5482235bc11ba65c71650fce
tatyderb <tatyderb@gmail.com>
2024-10-14 22:25:30 +0300

    Close window by X press or key q press.

--- src/application.py
+++ src/application.py
@@ -12,7 +12,9 @@ class Application:
     def run(self):
         running = True
         while running:
-            pass
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_qP:
+                    running = False
 
 
 def main():
