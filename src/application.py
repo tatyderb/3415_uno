395d9d34bb16b8f42fdad896e809bc951b7684d3
tatyderb <tatyderb@gmail.com>
2024-10-14 22:21:00 +0300

    Black window without reactions.

--- src/application.py
+++ src/application.py
@@ -1,17 +1,25 @@
+import pygame as pygame
+
 from ui.resource import RESOURCE as RSC
 
 
 class Application:
     def __init__(self):
-        pass
+        pygame.init()
+        self.size = (self.width, self.height) = (RSC['width'], RSC['height'])
+        self.display = pygame.display.set_mode(self.size)
 
     def run(self):
-        pass
+        running = True
+        while running:
+            pass
+
 
 def main():
     print('Start UNO application')
     app = Application()
     app.run()
+
 
 if __name__ == '__main__':
     main()
