fa8de20db4e84989919765da4b2229fed61cce37
tatyderb <tatyderb@gmail.com>
2024-10-14 22:13:07 +0300

    Структура класса Application

---
+++ src/application.py
@@ -0,0 +1,17 @@
+from ui.resource import RESOURCE as RSC
+
+
+class Application:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+def main():
+    print('Start UNO application')
+    app = Application()
+    app.run()
+
+if __name__ == '__main__':
+    main()
